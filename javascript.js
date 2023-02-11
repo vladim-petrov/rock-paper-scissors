7ef3152d74c992e00d01fa55de7d3f3ec03f7f1a
vladim-petrov <122881312+vladim-petrov@users.noreply.github.com>
2023-02-11 16:34:33 +0200

    Add player input nad function playRound

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// funcion CPU choice at random (Rock, paper, scissors)
+// get CPU choice at random (Rock, paper, scissors)
     const rock = 1;
     const paper = 2;
     const scissors = 3;
@@ -14,4 +14,57 @@ function getComputerChoice() {
     }
     return randomChoice;
 }
+
+// get player input (case-insensitive)
+const playerSelection = parseInt(prompt("Choose: rock, paper or scissors","Type: 1 for Rock, 2 for Paper or 3 for Scissors").toLowerCase());
+const computerSelection = getComputerChoice();
+
+// Write the logic of rock, paper, scissors game
+//
+
+function playRound(playerSelection, computerSelection) {
+    let winner;
+   if (playerSelection === rock && computerSelection === paper) {
+    return winner = "You Lose! Paper beats Rock";
+   } else if (playerSelection === rock && computerSelection === scissors) { 
+    return winner = "You Win! Rock beats Scissors";
+   } else if (playerSelection === rock && computerSelection === rock) { 
+    return winner = "Tie! Rock can't beat Rock";
+   } else if (playerSelection === paper && computerSelection === rock) { 
+    return winner = "You Win! Paper beats Rock";
+   } else if (playerSelection === paper && computerSelection === paper) { 
+    return winner = "Tie! Paper can't beat Paper";
+   } else if (playerSelection === paper && computerSelection === scissors) { 
+    return winner = "You Lose! Scissors beats Paper";
+   } else if (playerSelection === scissors && computerSelection === rock) { 
+    return winner = "You Lose! Rock beats Scissors";
+   } else if (playerSelection === scissors && computerSelection === paper) { 
+    return winner = "You win! Scissors beats Paper";
+   } else if (playerSelection === scissors && computerSelection === scissors) { 
+    return winner = "Tie! Scissors can't beat Scissors";
+   }
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
